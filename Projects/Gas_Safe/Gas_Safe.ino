07ef08a9f48dc99cd7cecbd47ccf17776869a0e5
Administrator <ryujt658@gmail.com>
2015-04-08 01:20:13 +0900

    Gas_Safe.ino: 소스 다듬기

--- Projects/Gas_Safe/Gas_Safe.ino
+++ Projects/Gas_Safe/Gas_Safe.ino
@@ -1,11 +1,24 @@
-int pinEcho = 2;
-int pinTrigger = 3;
-int pinObjectDetected = 4;
+// LED Digit Display
+int pinData = 2; 
+int pinClock = 3;
+int pinLatch = 4;
+
+// Two Button Module
 int pinButtonOn = 5;
 int pinButtonOff = 6;
+
+// LED for valve On/Off
 int pinValveOn = 7;
 int pinValveOff = 8;
-int pinBuzzer = 9;
+
+int pinBuzzer = 9; 
+
+// Ultra Sonic Sensor
+int pinEcho = 10;
+int pinTrigger = 11;
+
+// LED for Ultra Sonic Sensor
+int pinObjectDetected = 12;
 
 int timer_count_limit = 9 * 1000;
 
@@ -18,32 +31,42 @@ unsigned long oldMillis = 0;
 void setup() {
   Serial.begin(9600);
 
-  pinMode(pinEcho, OUTPUT);
-  pinMode(pinTrigger, INPUT);
-  pinMode(pinObjectDetected, OUTPUT);
+  pinMode(pinData, OUTPUT);
+  pinMode(pinClock, OUTPUT);  
+  pinMode(pinLatch, OUTPUT);
+
   pinMode(pinButtonOn, INPUT);
   pinMode(pinButtonOff, INPUT);
+  
   pinMode(pinValveOn, OUTPUT);
   pinMode(pinValveOff, OUTPUT);
+  
   pinMode(pinBuzzer, OUTPUT);  
+  
+  pinMode(pinEcho, INPUT);
+  pinMode(pinTrigger, OUTPUT);
+  pinMode(pinObjectDetected, OUTPUT);
   
   oldMillis = millis();
 }
 
 void loop() {
   check_ObjectDetected();
-  check_ValueButton();
+  check_ValveButton();
   check_timer();
   check_value();
 }
 
 void check_ObjectDetected()
 {
-  digitalWrite(pinEcho, HIGH); 
+  digitalWrite(pinTrigger, HIGH); 
   delayMicroseconds(10); 
-  digitalWrite(pinEcho, LOW);
+  digitalWrite(pinTrigger, LOW);
   
-  long duration = pulseIn(pinTrigger, HIGH); 
+  long duration = pulseIn(pinEcho, HIGH); 
+  
+  Serial.print(microsecondsToCentimeters(duration));
+  Serial.println();
   
   isObjectDetected = microsecondsToCentimeters(duration) <= 30;
   
@@ -51,7 +74,7 @@ void check_ObjectDetected()
   else digitalWrite(pinObjectDetected, LOW);
 }
 
-void check_ValueButton()
+void check_ValveButton()
 {
   if (digitalRead(pinButtonOn)  == LOW) isValveOpen = true;
   if (digitalRead(pinButtonOff) == LOW) isValveOpen = false;
@@ -75,8 +98,7 @@ void check_timer()
     timer_count = timer_count + (currentMillis - oldMillis);
   }
   
-  Serial.print(timer_count / 1000);
-  Serial.println();  
+  displayNumber(timer_count / 1000);
   
   oldMillis = currentMillis;
 }
@@ -128,4 +150,28 @@ void alarm(int pinNo) {
 			beep(pinNo, i, 11);
 		}
 	}
+}
+
+void displayNumber(int number)
+{
+  if (number > 9) number = 9;
+  
+  byte digits[10] = {
+    0b11000000,  // 0
+    0b11111001,  // 1
+    0b10100100,  // 2
+    0b10110000,  // 3
+    0b10011001,  // 4
+    0b10010010,  // 5
+    0b10000010,  // 6
+    0b11011000,  // 7
+    0b10000000,  // 8
+    0b10011000   // 9
+  };
+
+  byte data = digits[number];
+  
+  shiftOut(pinData, pinClock, MSBFIRST, data);
+  digitalWrite(pinLatch, HIGH);
+  digitalWrite(pinLatch, LOW);
 }
