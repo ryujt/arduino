910307ce87681817dd20ec06fede1f479f25f0c0
Administrator <ryujt658@gmail.com>
2015-05-11 17:39:21 +0900

    Rename IR_2 --> IR_Remote_Control

---
+++ Test/IR_Remote_Control/IR_Remote_Control.ino
@@ -0,0 +1,42 @@
+#include <IRremote.h>
+
+#define UP_KEY     0xFD8877
+#define DOWN_KEY   0xFD9867
+#define LEFT_KEY   0xFD28D7
+#define RIGHT_KEY  0xFD6897
+#define OK_KEY     0xFDA857
+#define STAR_KEY   0xFD30CF
+#define SHARP_KEY  0xFD708F
+
+const int pin_ir = 6;
+
+IRrecv irrecv(pin_ir);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+}
+
+void loop()
+{
+  if (irrecv.decode(&results)) {
+    Serial.print(results.value, HEX);
+    Serial.print(": ");
+    
+    switch(results.value) {
+      case UP_KEY:     Serial.println("UP_KEY");     break;
+      case DOWN_KEY:   Serial.println("DOWN_KEY");   break;
+      case LEFT_KEY:   Serial.println("LEFT_KEY");   break;
+      case RIGHT_KEY:  Serial.println("RIGHT_KEY");  break;
+      case OK_KEY:     Serial.println("OK_KEY");     break;
+      case STAR_KEY:   Serial.println("STAR_KEY");   break;
+      case SHARP_KEY:  Serial.println("UP_KEY");     break;
+      default: Serial.println();
+    }
+    
+    irrecv.resume();
+  }
+}
