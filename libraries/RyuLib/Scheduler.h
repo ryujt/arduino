6f8331256795d8ff2efe240040cffeae28b5bb78
Administrator <ryujt658@gmail.com>
2015-05-21 12:45:29 +0900

    Add hardware folder and update libraries folder.

---
+++ libraries/RyuLib/Scheduler.h
@@ -0,0 +1,24 @@
+class Listener {
+
+protected:
+  void on_Interval() = 0;
+  void on_Timer() = 0;
+  
+public:
+  void execute() = 0;  
+};
+
+class Scheduler {
+private:
+  // std::Vector<Listener> *_listeners;
+public:
+  // void addListener(Listener *pListener)
+  // {
+  //   _listener = pListener;
+  // }
+  
+  // void tick()
+  // {
+  //   _listener->execute();
+  // }
+};
