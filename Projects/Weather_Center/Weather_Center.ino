c723a26377408d27955e0a3a688e740ee61a0f31
Administrator <ryujt658@gmail.com>
2015-07-21 00:00:32 +0900

    간이 기상 센터 프로젝트 배선표

--- Projects/Weather_Center/Weather_Center.ino
+++ Projects/Weather_Center/Weather_Center.ino
@@ -6,35 +6,37 @@
 LiquidCrystal_I2C lcd(0x27,16,2);
 DHT dht(3, DHT11); 
 
-int isEthernetOk = 1;
-
-int dustPin = 0;
- 
-int ledPower = 5;
-int delayTime = 280;
-int delayTime2 = 40;
-float offTime = 9680;
-
 static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 byte Ethernet::buffer[512];
 
-static void my_callback (byte status, word off, word len) {
-  Serial.println("http received:");
-  Serial.print((const char*) Ethernet::buffer + off);
-  Serial.println("...");
-}
+int isEthernetOk = 0;
+
+int sendInterval = 50;
+int sendCount = 0;
+
+int pinDustSensor = 0;
+int pinDustSensorLED = 5;
+int pinEthernetSwitch = 7;
 
 void setup()
 {
   Serial.begin(57600);
   Serial.println("setup begin!");
 
+  pinMode(pinDustSensorLED, OUTPUT);
+  pinMode(pinEthernetSwitch, INPUT_PULLUP);
+
   lcd.init();                     
   lcd.backlight();
   lcd.print("setup begin!");
 
-  pinMode(ledPower, OUTPUT);
+  isEthernetOk = (digitalRead(pinEthernetSwitch) == LOW);
 
+  if (isEthernetOk) prepareEthernet();
+}
+
+void prepareEthernet()
+{
   lcd.clear();
   lcd.print("Ethernet start");
   if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) {
@@ -42,11 +44,13 @@ void setup()
     Serial.println("Failed to access Ethernet controller");
   }
 
-  lcd.clear();
-  lcd.print("DHCP start");
-  if ((isEthernetOk) && (!ether.dhcpSetup())) {
-    isEthernetOk = 0;
-    Serial.println("DHCP failed");
+  if (isEthernetOk) {
+    lcd.clear();
+    lcd.print("DHCP start");
+    if (!ether.dhcpSetup()) {
+      isEthernetOk = 0;
+      Serial.println("DHCP failed");
+    }
   }
 
   if (isEthernetOk) {
@@ -61,8 +65,6 @@ void setup()
 
 void loop()
 {
-  Serial.println("loop start!");
-
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
@@ -75,34 +77,49 @@ void loop()
   
   //////////////////////////////////
 
-  digitalWrite(ledPower, HIGH); 
+  int delayTime = 280;
+  int delayTime2 = 40;
+  float offTime = 9680;
+
+  digitalWrite(pinDustSensorLED, HIGH); 
   delayMicroseconds(delayTime);
 
-  float dustVal = analogRead(dustPin);
+  float dustVal = analogRead(pinDustSensor);
   delayMicroseconds(delayTime2);
 
-  digitalWrite(ledPower, LOW);
+  digitalWrite(pinDustSensorLED, LOW);
   delayMicroseconds(offTime);
 
-  float d = 0.17 * (dustVal * 0.0049) - 0.1;
+  float d = (0.17 * (dustVal * 0.0049) - 0.1) * 1000;
   if (d < 0) d = 0;
 
   lcd.setCursor(0, 1);
   lcd.print("D=");
-  lcd.print(d);
-  lcd.print(" (mg/m3)");
+  lcd.print(dustVal);
+  lcd.print(" (ug/m3)      ");
 
   //////////////////////////////////
 
   if (isEthernetOk) {
-    Serial.println("http send");
-    
-    char pMsg[64]; 
-    sprintf("?t=%f&h=%f&d=%f", pMsg, t, h, d);
-    ether.browseUrl(PSTR("/weather/set_data"), pMsg, "www.helloworld.or.kr", my_callback);
-    ether.packetLoop(ether.packetReceive());
+    sendCount = sendCount + 1;
+    if (sendCount >= sendInterval) {
+      sendCount = 0;
+      
+      Serial.println("http send");
+      
+      char pMsg[64]; 
+      sprintf("?t=%f&h=%f&d=%f", pMsg, t, h, d);
+      ether.browseUrl(PSTR("/weather/set_data"), pMsg, "www.helloworld.or.kr", my_callback);
+      ether.packetLoop(ether.packetReceive());
+    }
   }
   
-  delay(1000); 
+  delay(100); 
+}
+
+static void my_callback (byte status, word off, word len) {
+  Serial.println("http received:");
+  Serial.print((const char*) Ethernet::buffer + off);
+  Serial.println("...");
 }
 
