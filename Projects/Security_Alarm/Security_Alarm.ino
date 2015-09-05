e6f5db214cb58b201f9d0fcd8b48c66f8e008418
Administrator <ryujt658@gmail.com>
2015-09-06 00:58:32 +0900

    Add project Thermal Printer

--- Projects/Security_Alarm/Security_Alarm.ino
+++ Projects/Security_Alarm/Security_Alarm.ino
@@ -1,37 +1,36 @@
- ./*
+ /*
  * PIR sensor tester
  */
  
-int ledPin = 13;                // choose the pin for the LED
-int inputPin = 4;               // choose the input pin (for PIR sensor)
-int pirState = LOW;             // we start, assuming no motion detected
-int val = 0;                    // variable for reading the pin status
+int buzzerPin = 0;
+int inputPin = 2; 
+int ledPin = 3;   
+int swPin = 4;   
+
+int pirState = LOW;
+int swOn = false;
  
 void setup() {
-  pinMode(ledPin, OUTPUT);      // declare LED as output
-  pinMode(inputPin, INPUT);     // declare sensor as input
-  pinMode(8, OUTPUT);
-
-  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);   
+  pinMode(inputPin, INPUT);  
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(swPin, INPUT);  
 }
- 
-void loop(){
-  val = digitalRead(inputPin);  // read input value
-  if (val == HIGH) {            // check if the input is HIGH
-    digitalWrite(ledPin, HIGH);  // turn LED ON
-    seAlarm(8);
+
+void loop() {
+  if (digitalRead(swPin) == LOW) swOn = true;
+  
+  if (digitalRead(inputPin) == HIGH) {       
+    digitalWrite(ledPin, HIGH);
+
     if (pirState == LOW) {
-      // we have just turned on
-      Serial.println("Motion detected!");
-      // We only want to print on the output change, not state
+      if (swOn) seAlarm(buzzerPin);
       pirState = HIGH;
     }
   } else {
-    digitalWrite(ledPin, LOW); // turn LED OFF
+    digitalWrite(ledPin, LOW);
+
     if (pirState == HIGH){
-      // we have just turned of
-      Serial.println("Motion ended!");
-      // We only want to print on the output change, not state
       pirState = LOW;
     }
   }
@@ -39,13 +38,9 @@ void loop(){
 
 void beep(int speakerPin, float noteFrequency, long noteDuration) {
   int x;
-  // Convert the frequency to microseconds
   float microsecondsPerWave = 1000000 / noteFrequency;
-  // Calculate how many milliseconds there are per HIGH/LOW cycles.
   float millisecondsPerCycle = 1000 / (microsecondsPerWave * 2);
-  // Multiply noteDuration * number or cycles per millisecond
   float loopTime = noteDuration * millisecondsPerCycle;
-  // Play the note for the calculated loopTime.
   for (x = 0; x < loopTime; x++) {
     digitalWrite(speakerPin, HIGH);
     delayMicroseconds(microsecondsPerWave);
---
+++ Test/Thrermal_Printer/Thrermal_Printer.ino
@@ -0,0 +1,104 @@
+#include <SoftwareSerial.h>
+#include <Adafruit_Thermal.h>
+#include "adalogo.h"
+#include "adaqrcode.h"
+
+SoftwareSerial Thermal(5, 6);
+Adafruit_Thermal printer(&Thermal);
+
+int heatTime = 80;
+int heatInterval = 255;
+char printDensity = 15; 
+char printBreakTime = 15;
+
+void setup() {
+  Thermal.begin(9600);
+  printer.begin(); 
+  
+  // Test inverse on & off
+  printer.inverseOn();
+  printer.println(F("Inverse ON"));
+  printer.inverseOff();
+
+  // Test character double-height on & off
+  printer.doubleHeightOn();
+  printer.println(F("Double Height ON"));
+  printer.doubleHeightOff();
+
+  // Set text justification (right, center, left) -- accepts 'L', 'C', 'R'
+  printer.justify('R');
+  printer.println(F("Right justified"));
+  printer.justify('C');
+  printer.println(F("Center justified"));
+  printer.justify('L');
+  printer.println(F("Left justified"));
+
+  // Test more styles
+  printer.boldOn();
+  printer.println(F("Bold text"));
+  printer.boldOff();
+
+  printer.underlineOn();
+  printer.println(F("Underlined text"));
+  printer.underlineOff();
+
+  printer.setSize('L');        // Set type size, accepts 'S', 'M', 'L'
+  printer.println(F("Large"));
+  printer.setSize('M');
+  printer.println(F("Medium"));
+  printer.setSize('S');
+  printer.println(F("Small"));
+
+  printer.justify('C');
+  printer.println(F("normal\nline\nspacing"));
+  printer.setLineHeight(50);
+  printer.println(F("Taller\nline\nspacing"));
+  printer.setLineHeight(); // Reset to default
+  printer.justify('L');
+
+  // Barcode examples:
+  // CODE39 is the most common alphanumeric barcode:
+  printer.printBarcode("ADAFRUT", CODE39);
+  printer.setBarcodeHeight(100);
+  // Print UPC line on product barcodes:
+  printer.printBarcode("123456789123", UPC_A);
+
+  // Print the 75x75 pixel logo in adalogo.h:
+  printer.printBitmap(adalogo_width, adalogo_height, adalogo_data);
+
+  // Print the 135x135 pixel QR code in adaqrcode.h:
+  printer.printBitmap(adaqrcode_width, adaqrcode_height, adaqrcode_data);
+  printer.println(F("Adafruit!"));
+  printer.feed(2);
+  
+  printer.sleep();      // Tell printer to sleep
+  delay(3000L);         // Sleep for 3 seconds
+  printer.wake();       // MUST wake() before printing again, even if reset
+  printer.setDefault(); // Restore printer to defaults
+}
+
+void initPrinter() {
+  //Modify the print speed and heat
+  Thermal.write(27);
+  Thermal.write(55);
+  Thermal.write(7); //Default 64 dots = 8*('7'+1)
+  Thermal.write(heatTime); //Default 80 or 800us
+  Thermal.write(heatInterval); //Default 2 or 20us
+  
+  //Modify the print density and timeout
+  Thermal.write(18);
+  Thermal.write(35);
+  
+  int printSetting = (printDensity<<4) | printBreakTime;
+  Thermal.write(printSetting); //Combination of printDensity and printBreakTime
+}
+
+void loop() {
+//  Thermal.println(" ... ");
+//  Thermal.write(10); //Sends the LF to the printer, advances the paper
+//  Thermal.print(" Millis = ");
+//  Thermal.println(millis()); 
+//  Thermal.write(10);
+//  Thermal.write(10); 
+//  do { } while (1>0);
+}
