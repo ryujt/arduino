be16ea2e1ac9d73589710a3d716eac8963af79ac
Administrator <ryujt658@gmail.com>
2015-12-06 14:47:03 +0900

    Night_Light 소스 수정 및 사진 추가

--- Projects/Night_Light/Night_Light.ino
+++ Projects/Night_Light/Night_Light.ino
@@ -1,15 +1,19 @@
 int pin_LED = 1;
-int pin_sensor = 2;
+int pin_sensor = 3;
 
 void setup() {
+//  Serial.begin(9600);
+  
   pinMode(pin_LED, OUTPUT);
   pinMode(pin_sensor, INPUT);
 }
 
 void loop() {
   int value = analogRead(pin_sensor);
+  
+//  Serial.println(value);
 
-  if (value < 100) digitalWrite(pin_LED, HIGH);
+  if (value > 900) digitalWrite(pin_LED, HIGH);
   else digitalWrite(pin_LED, LOW);
   
   delay(100);
