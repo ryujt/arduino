e36d7c791f57a867ea235deb77e1d087423a3180
Administrator <ryujt658@gmail.com>
2015-12-19 23:18:28 +0900

    걷는 동작 개선 (배터리 교체 후, 모든 서보 모터를 함께 구동)

--- Projects/Quadruped_Robot/Quadruped_Robot.ino
+++ Projects/Quadruped_Robot/Quadruped_Robot.ino
@@ -4,22 +4,24 @@
 #include <Servo.h> 
 
 #define servo_count 8
+#define delay_interval 100
 
 Servo servos[servo_count];
 
 int start_pos[servo_count] = {
    90,   90,   90,  90,
-   40, 140, 155,  40
+   40, 150, 155,  40
 };
 
 void setup() {
+  Serial.begin(9600);
+  
   for (int i=0; i<servo_count; i++) {
     servos[i].attach(i+2); 
     servos[i].write(start_pos[i]); 
-    
-    delay(500);
-    servos[i].detach(); 
   }
+  
+  delay(1000);
 }
 
 void loop() {
@@ -36,60 +38,60 @@ void loop() {
 }
 
 void set_angle(int no, float angle) {
-    servos[no].attach(no+2); 
     servos[no].write(angle); 
-    delay(100);
-    servos[no].detach(); 
 }
 
-void set_start_position() {
-  for (int i=0; i<4; i++) {
-    servos[i].attach(i+2); 
-    servos[i].write(start_pos[i]); 
+void push_legs() {
+  int positions[4] = {50, 130, 50, 130};
+  
+  for (int j=1; j<=4; j++) {
+    for (int i=0; i<4; i++) {
+      if ((i % 2) == 0) {
+        servos[i].write(positions[i] + 20 * j);
+        Serial.println(positions[i] + 20 * j);
+      } else {
+        servos[i].write(positions[i] - 20 * j);
+        Serial.println(positions[i] - 20 * j);
+      }
+    }
+    
+      delay(delay_interval);
   }
-}
-
-void right_front_start() {
-  set_angle(5, start_pos[5]);
-  set_angle(1, start_pos[1]);
-}
-
-void right_back_start() {
-  set_angle(7, start_pos[7]);
-  set_angle(3, start_pos[3]);
 }
 
 void right_front_forward() {
   set_angle(5, start_pos[5] - 40);
   set_angle(1, start_pos[1] + 40);
+  
+  delay(delay_interval);
+  
   set_angle(5, start_pos[5]);
 }
 
 void right_back_forward() {
   set_angle(7, start_pos[7] + 40);
   set_angle(3, start_pos[3] + 40);
+  
+  delay(delay_interval);
+  
   set_angle(7, start_pos[7]);
-}
-
-void left_front_start() {
-  set_angle(4, start_pos[4]);
-  set_angle(0, start_pos[0]);
-}
-
-void left_back_start() {
-  set_angle(6, start_pos[6]);
-  set_angle(2, start_pos[2]);
 }
 
 void left_front_forward() {
   set_angle(4, start_pos[4] + 40);
   set_angle(0, start_pos[0] - 40);
+  
+  delay(delay_interval);
+  
   set_angle(4, start_pos[4]);
 }
 
 void left_back_forward() {
   set_angle(6, start_pos[6] - 40);
   set_angle(2, start_pos[2] - 40);
+  
+  delay(delay_interval);
+  
   set_angle(6, start_pos[6]);
 }
 
@@ -97,13 +99,13 @@ void go_forward() {
   right_front_forward();
   left_front_forward();
   
+  delay(delay_interval);
+  
   right_back_forward();
   left_back_forward();
 
-  right_front_start();
-  left_front_start();
-
-  right_back_start();
-  left_back_start();
+  delay(delay_interval);
+  
+  push_legs();
 }
 
