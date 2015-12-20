ca6748c4a4b59a163352c3d1f85bb1c6fc26596f
Administrator <ryujt658@gmail.com>
2015-12-20 22:36:18 +0900

    Add TM1637

---
+++ Test/TM1637/TM1637.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <TM1637Display.h>
+
+#define CLK 2
+#define DIO 3
+
+TM1637Display display(CLK, DIO);
+
+void setup() {
+  display.setBrightness(0x0f);
+}
+
+void loop() {
+  for (int i=0; i<9999; i++) {
+    display.showNumberDec(i, true, 4, 0);
+  }
+}
+
