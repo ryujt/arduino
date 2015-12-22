7825e9f6dbc560d3e17f83f3461c764e2247efcd
Administrator <ryujt658@gmail.com>
2015-12-22 09:02:22 +0900

    RC-Car 예제 변경

--- class room/아빠와 함께하는 아두이노/Chapter 3. RC 자동차 만들기/_04._RC_Car/_04._RC_Car.ino
+++ class room/아빠와 함께하는 아두이노/Chapter 3. RC 자동차 만들기/_04._RC_Car/_04._RC_Car.ino
@@ -1,28 +1,67 @@
-#include <IRremote.h>
-#include "motor.h"
+#include &lt;IRremote.h>
 
+#define AIA 5
+#define AIB 6
+#define BIA 3
+#define BIB 4
+    
 IRrecv irrecv(7);
-Motor motor(3, 4, 5, 6);
 
-void setup()
-{
-  Serial.begin(9600);
+void setup() {
   irrecv.enableIRIn();
+
+  pinMode(AIA, OUTPUT);
+  pinMode(AIB, OUTPUT);
+  pinMode(BIA, OUTPUT);
+  pinMode(BIB, OUTPUT);
+  
+  do_stop();  
 }
 
-void loop()
-{
+void loop() {
   decode_results results;
   boolean has_result = irrecv.decode(&results);
   if (has_result) {
-    Serial.println(results.value);
-    
-    if (results.value == 16623703) motor.stop();
-    if (results.value == 16615543) motor.forward();
-    if (results.value == 16619623) motor.backward();
-    if (results.value == 16591063) motor.left();
-    if (results.value == 16607383) motor.right();
-    
+    if (results.value == 16623703) do_stop();
+    if (results.value == 16615543) forward();
+    if (results.value == 16619623) backward();
+    if (results.value == 16591063) left();
+    if (results.value == 16607383) right();
     irrecv.resume();
   }
+}
+
+void forward() {
+  digitalWrite(AIA, HIGH);
+  digitalWrite(AIB, LOW);
+  digitalWrite(BIA, HIGH);
+  digitalWrite(BIB, LOW);
+}
+
+void backward() {
+  digitalWrite(AIA, LOW);
+  digitalWrite(AIB, HIGH);
+  digitalWrite(BIA, LOW);
+  digitalWrite(BIB, HIGH);
+}
+
+void left() {
+  digitalWrite(AIA, HIGH);
+  digitalWrite(AIB, LOW);
+  digitalWrite(BIA, LOW);
+  digitalWrite(BIB, HIGH);
+}
+
+void right() {
+  digitalWrite(AIA, LOW);
+  digitalWrite(AIB, HIGH);
+  digitalWrite(BIA, HIGH);
+  digitalWrite(BIB, LOW);
+}
+
+void do_stop() {
+  digitalWrite(AIA, LOW);
+  digitalWrite(AIB, LOW);
+  digitalWrite(BIA, LOW);
+  digitalWrite(BIB, LOW);
 }
