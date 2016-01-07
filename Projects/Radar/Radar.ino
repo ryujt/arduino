f9500a30614ebd2116c84377277b067cded64a62
Administrator <ryujt658@gmail.com>
2016-01-08 08:08:10 +0900

    Radar 프로젝트에 초음파 센서와 서보 모터 연결 https://youtu.be/G-bxmV88Z-E

--- Projects/Radar/Radar.ino
+++ Projects/Radar/Radar.ino
@@ -2,14 +2,22 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 #include <math.h>
+#include <Servo.h> 
 
 #define  PI  3.141592654
 
 Adafruit_PCD8544 lcd = Adafruit_PCD8544(7, 6, 5, 4, 3);
 
+Servo servo; 
+
+int pin_echo = 9;
+int pin_trig = 10;
+
 int distances[19];
 
 void setup() {
+  Serial.begin(9600);
+  
   memset(&distances[0], 0, sizeof(distances));
   
   srand(analogRead(0));
@@ -17,20 +25,53 @@ void setup() {
   lcd.begin();
   lcd.setContrast(50);
   lcd.clearDisplay();
+
+  servo.attach(8); 
+
+  pinMode(pin_echo, INPUT); 
+  pinMode(pin_trig, OUTPUT); 
 }
 
 void loop() {
-  for (int i=0; i<19; i++) {
+  for (int i=0; i<19; i++) {    
     int angle = i * 10;
-    distances[i] = (rand() % 10) + 31;
+
+    servo.write(angle);
+    
+    long duration, cm;
+    digitalWrite(pin_trig, HIGH); 
+    delay(10); 
+    digitalWrite(pin_trig, LOW);
+
+    duration = pulseIn(pin_echo, HIGH); 
+    cm = microsecondsToCentimeters(duration); 
+
+    Serial.println(cm);
+
+    distances[i] = cm ;
     draw_radar();
+  
     delay(250);
   }
 
   for (int i=(19-1); i>=0; i--) {
     int angle = i * 10;
-    distances[i] = (rand() % 10) + 31;
+
+    servo.write(angle);
+    
+    long duration, cm;
+    digitalWrite(pin_trig, HIGH); 
+    delay(10); 
+    digitalWrite(pin_trig, LOW);
+
+    duration = pulseIn(pin_echo, HIGH); 
+    cm = microsecondsToCentimeters(duration); 
+
+    Serial.println(cm);
+
+    distances[i] = cm ;
     draw_radar();
+    
     delay(250);
   }
 }
@@ -50,4 +91,14 @@ void draw_radar() {
     for (int i=1; i<10; i++) lcd.drawCircle(42, 48, i*5, BLACK);
     for (int i=0; i<19; i++) lcd.fillCircle(get_x(i, distances[i]), get_y(i, distances[i]), 2, BLACK);
     lcd.display();
+}
+
+long microsecondsToInches(long microseconds)
+{
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds / 29 / 2;
 }
