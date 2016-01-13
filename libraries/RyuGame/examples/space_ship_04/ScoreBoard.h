d625eb9931acac0551fc72df6680510bd3d9eb42
Administrator <ryujt658@gmail.com>
2016-01-13 10:12:35 +0900

    점수가 너무 빨리 마이너스 표시 되는 것을 수정

--- libraries/RyuGame/examples/space_ship_04/ScoreBoard.h
+++ libraries/RyuGame/examples/space_ship_04/ScoreBoard.h
@@ -5,6 +5,7 @@
 class ScoreBoard : public GameControlBase
 {
 private:
+        int _ScoreTemp = 0;
         int _Score = 0;
 public:
 	void start() 
@@ -13,10 +14,16 @@ public:
 
 	void update(unsigned long tick) 
 	{
-                if (is_game_end == false) _Score = _Score + tick;
+                if (is_game_end == false) {
+                        _ScoreTemp = _ScoreTemp + tick;
+                        if (_ScoreTemp >= 100) {
+                                _Score = _Score + (_ScoreTemp / 100);
+                                _ScoreTemp = _ScoreTemp % 100;
+                        }
+                }
 
                 _LCD->setTextSize(1);
                 _LCD->setCursor(0,0);
-                _LCD->println(_Score / 100);                
+                _LCD->println(_Score);                
 	}
 };
