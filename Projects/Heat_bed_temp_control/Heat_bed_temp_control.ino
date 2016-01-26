a64297935d9a931c76855a3895aadc5bc7df0bad
Administrator <ryujt658@gmail.com>
2016-01-27 03:44:35 +0900

    파일 정리

--- Projects/Heat_bed_temp_control/Heat_bed_temp_control.ino
+++ Projects/Heat_bed_temp_control/Heat_bed_temp_control.ino
@@ -7,6 +7,8 @@ int target_temp = 65;
 
 int pin_temp = 0;
 int pin_relay = 4;
+int pin_sw_01 = 8;
+int pin_sw_02 = 9;
 int pin_led = 13;
 
 TM1637Display display(CLK, DIO);
@@ -14,6 +16,9 @@ TM1637Display display(CLK, DIO);
 void setup() {
   Serial.begin(9600);
   
+  pinMode(pin_sw_01, INPUT_PULLUP);
+  pinMode(pin_sw_02, INPUT_PULLUP);
+
   pinMode(pin_relay, OUTPUT);
   pinMode(pin_led, OUTPUT);
   
@@ -22,10 +27,25 @@ void setup() {
 }
 
 void loop() {
+  if (digitalRead(pin_sw_01) == LOW) {
+    target_temp = 80;
+    display.showNumberDec(target_temp, true, 4, 0);
+    return;
+  }
+  
+  if (digitalRead(pin_sw_02) == LOW) {
+    target_temp = 65; 
+    display.showNumberDec(target_temp, true, 4, 0);
+    return;
+  }
+  
   int tempReading = analogRead(pin_temp);
   float tempVolts = tempReading * 5.0 / 1024.0;
   float tempC = (tempVolts - 0.5) * 100.0;
 
+  Serial.print("target_temp: ");
+  Serial.print(target_temp);
+  Serial.print(", tempC: ");
   Serial.println(tempC);
 
   display.showNumberDec(tempC, true, 4, 0);
