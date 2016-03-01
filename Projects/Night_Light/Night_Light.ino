53ea33239f1a69fecf726a49bc2df692144e0a5a
Administrator <ryujt658@gmail.com>
2016-03-02 05:35:07 +0900

    Upgrade night light project for ATTiny85.

--- Projects/Night_Light/Night_Light.ino
+++ Projects/Night_Light/Night_Light.ino
@@ -1,20 +1,24 @@
-int pin_LED = 13;
-int pin_sensor = 5;
+int pin_LED_LOW = 0;
+int pin_LED_HIGH = 1;
+int pin_sensor = 3;
 
 void setup() {
-  Serial.begin(9600);
-  
-  pinMode(pin_LED, OUTPUT);
-  pinMode(pin_sensor, INPUT);
+  //Serial.begin(9600);
+
+  pinMode(pin_LED_LOW, OUTPUT);
+  pinMode(pin_LED_HIGH, OUTPUT);
+  pinMode(pin_sensor, INPUT);  
+
+  digitalWrite(pin_LED_LOW, LOW);
 }
 
 void loop() {
   int value = analogRead(pin_sensor);
   
-  Serial.println(value);
+  //Serial.println(value);
 
-  if (value > 200) digitalWrite(pin_LED, HIGH);
-  else digitalWrite(pin_LED, LOW);
+  if (value > 900) digitalWrite(pin_LED_HIGH, HIGH);
+  else digitalWrite(pin_LED_HIGH, LOW);
   
   delay(100);
 }
